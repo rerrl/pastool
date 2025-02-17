7966fec894acb07b83e9754ec27948c602519572
rerrl <avgutierrez@protonmail.com>
2025-02-17 10:04:34 -0800

    add password lenght and symbol flag

--- src/components/GenerateNewPass.tsx
+++ src/components/GenerateNewPass.tsx
@@ -1,44 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Checkbox from "./Checkbox";
 
 export default function GenerateNewPass() {
-  const [copyToClipboardOnCreate, setCopyToClipboardOnCreate] = useState(true);
+  // const [copyToClipboardOnCreate, setCopyToClipboardOnCreate] = useState(true);
   const [noSymbolOnPassword, setNoSymbolOnPassword] = useState(false);
+  const [passwordLength, setPasswordLength] = useState(25);
+
+  useEffect(() => {
+    console.log(passwordLength);
+  }, [passwordLength]);
 
   return (
     <div>
-      <div>Password Path</div>
-      <input
-        className="my-4"
-        type="text"
-        placeholder="Password Path"
-        value="~/.passwords/password.gpg"
-      />
-
-      <div>Password Length</div>
-      <input
-        className="my-4"
-        type="number"
-        placeholder="Password Length"
-        value="16"
-      />
+      <div className="flex items-center justify-center my-4 space-x-5">
+        <div>Password Length</div>
+        <input
+          className="my-4"
+          type="number"
+          placeholder="Password Length"
+          onChange={(e) =>
+            setPasswordLength(parseInt(e.target.value as string))
+          }
+          value={passwordLength}
+          min={1}
+          max={100}
+        />
+      </div>
       {/* checkboxes */}
       <div className="flex items-center justify-center my-4 space-x-5">
-        <Checkbox
-          checked={copyToClipboardOnCreate}
-          onChange={(e) => {
-            setCopyToClipboardOnCreate(e.target.checked);
-          }}
-          id="copy-to-clipboard"
-          value="Copy to Clipboard"
-        />
         <Checkbox
           checked={noSymbolOnPassword}
           onChange={(e) => {
             setNoSymbolOnPassword(e.target.checked);
           }}
-          id="no-symbol-on-password"
-          value="No Symbol on Password"
+          id="no-symbols"
+          value="No Symbols"
         />
       </div>
     </div>
